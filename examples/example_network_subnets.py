1bddf3018c6214e1d3419841f240f82896daf905
Zim Kalinowski <zikalino@microsoft.com>
2020-06-06 08:27:21 +0200

    subnets

---
+++ examples/example_network_subnets.py
@@ -0,0 +1,146 @@
+#-------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+#--------------------------------------------------------------------------
+
+import os
+from azure.mgmt.network import NetworkManagementClient
+from azure.mgmt.resource import ResourceManagementClient
+from azure.common.credentials import ServicePrincipalCredentials
+
+
+#--------------------------------------------------------------------------
+# credentials from environment
+#--------------------------------------------------------------------------
+SUBSCRIPTION_ID = os.environ['AZURE_SUBSCRIPTION_ID']
+TENANT_ID = os.environ['AZURE_TENANT']
+CLIENT_ID = os.environ['AZURE_CLIENT_ID']
+CLIENT_SECRET = os.environ['AZURE_SECRET']
+
+
+#--------------------------------------------------------------------------
+# variables
+#--------------------------------------------------------------------------
+AZURE_LOCATION = 'eastus'
+RESOURCE_GROUP = "myResourceGroup"
+VIRTUAL_NETWORK_NAME = "myVirtualNetwork"
+SUBNET_NAME = "mySubnet"
+
+
+#--------------------------------------------------------------------------
+# management clients
+#--------------------------------------------------------------------------
+credentials = ServicePrincipalCredentials(
+    client_id=CLIENT_ID,
+    secret=CLIENT_SECRET,
+    tenant=TENANT_ID
+)
+mgmt_client = NetworkManagementClient(credentials, SUBSCRIPTION_ID)
+resource_client = ResourceManagementClient(credentials, SUBSCRIPTION_ID)
+
+
+#--------------------------------------------------------------------------
+# resource group (prerequisite)
+#--------------------------------------------------------------------------
+print("Creating Resource Group")
+resource_client.resource_groups.create_or_update(resource_group_name=RESOURCE_GROUP, parameters={ 'location': AZURE_LOCATION })
+
+
+#--------------------------------------------------------------------------
+# /VirtualNetworks/put/Create virtual network[put]
+#--------------------------------------------------------------------------
+print("Create virtual network")
+BODY = {
+  "address_space": {
+    "address_prefixes": [
+      "10.0.0.0/16"
+    ]
+  },
+  "location": AZURE_LOCATION
+}
+result = mgmt_client.virtual_networks.create_or_update(resource_group_name=RESOURCE_GROUP, virtual_network_name=VIRTUAL_NETWORK_NAME, parameters=BODY)
+result = result.result()
+
+
+#--------------------------------------------------------------------------
+# /Subnets/put/Create subnet[put]
+#--------------------------------------------------------------------------
+print("Create subnet")
+BODY = {
+  "address_prefix": "10.0.0.0/16"
+}
+result = mgmt_client.subnets.create_or_update(resource_group_name=RESOURCE_GROUP, virtual_network_name=VIRTUAL_NETWORK_NAME, subnet_name=SUBNET_NAME, subnet_parameters=BODY)
+result = result.result()
+
+
+#--------------------------------------------------------------------------
+# /Subnets/put/Create subnet with service endpoints[put]
+#--------------------------------------------------------------------------
+print("Create subnet with service endpoints")
+BODY = {
+  "address_prefix": "10.0.0.0/16",
+  "service_endpoints": [
+    {
+      "service": "Microsoft.Storage"
+    }
+  ]
+}
+result = mgmt_client.subnets.create_or_update(resource_group_name=RESOURCE_GROUP, virtual_network_name=VIRTUAL_NETWORK_NAME, subnet_name=SUBNET_NAME, subnet_parameters=BODY)
+result = result.result()
+
+
+#--------------------------------------------------------------------------
+# /Subnets/put/Create subnet with a delegation[put]
+#--------------------------------------------------------------------------
+print("Create subnet with a delegation")
+BODY = {
+  "address_prefix": "10.0.0.0/16"
+}
+result = mgmt_client.subnets.create_or_update(resource_group_name=RESOURCE_GROUP, virtual_network_name=VIRTUAL_NETWORK_NAME, subnet_name=SUBNET_NAME, subnet_parameters=BODY)
+result = result.result()
+
+
+#--------------------------------------------------------------------------
+# /Subnets/get/Get subnet with a delegation[get]
+#--------------------------------------------------------------------------
+print("Get subnet with a delegation")
+result = mgmt_client.subnets.get(resource_group_name=RESOURCE_GROUP, virtual_network_name=VIRTUAL_NETWORK_NAME, subnet_name=SUBNET_NAME)
+
+
+#--------------------------------------------------------------------------
+# /Subnets/get/Get subnet[get]
+#--------------------------------------------------------------------------
+print("Get subnet")
+result = mgmt_client.subnets.get(resource_group_name=RESOURCE_GROUP, virtual_network_name=VIRTUAL_NETWORK_NAME, subnet_name=SUBNET_NAME)
+
+
+#--------------------------------------------------------------------------
+# /Subnets/get/List subnets[get]
+#--------------------------------------------------------------------------
+print("List subnets")
+result = mgmt_client.subnets.list(resource_group_name=RESOURCE_GROUP, virtual_network_name=VIRTUAL_NETWORK_NAME)
+
+
+#--------------------------------------------------------------------------
+# /Subnets/post/Unprepare Network Policies[post]
+#--------------------------------------------------------------------------
+print("Unprepare Network Policies")
+#result = mgmt_client.subnets.unprepare_network_policies(resource_group_name=RESOURCE_GROUP, virtual_network_name=VIRTUAL_NETWORK_NAME, subnet_name=SUBNET_NAME, service_name="Microsoft.Sql/managedInstances")
+#result = result.result()
+
+
+#--------------------------------------------------------------------------
+# /Subnets/post/Prepare Network Policies[post]
+#--------------------------------------------------------------------------
+print("Prepare Network Policies")
+#result = mgmt_client.subnets.prepare_network_policies(resource_group_name=RESOURCE_GROUP, virtual_network_name=VIRTUAL_NETWORK_NAME, subnet_name=SUBNET_NAME, service_name="Microsoft.Sql/managedInstances")
+#result = result.result()
+
+
+#--------------------------------------------------------------------------
+# /Subnets/delete/Delete subnet[delete]
+#--------------------------------------------------------------------------
+print("Delete subnet")
+result = mgmt_client.subnets.delete(resource_group_name=RESOURCE_GROUP, virtual_network_name=VIRTUAL_NETWORK_NAME, subnet_name=SUBNET_NAME)
+result = result.result()
