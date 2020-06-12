c823cae76956ef7b0791f5971d2625c57b5d0b1e
Zim Kalinowski <zikalino@microsoft.com>
2020-06-12 13:33:40 +0200

    operations

---
+++ examples/example_network_operations.py
@@ -0,0 +1,43 @@
+#-------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+#--------------------------------------------------------------------------
+
+import os
+from azure.mgmt.network import NetworkManagementClient
+from azure.common.credentials import ServicePrincipalCredentials
+
+
+#--------------------------------------------------------------------------
+# credentials from environment
+#--------------------------------------------------------------------------
+SUBSCRIPTION_ID = os.environ['AZURE_SUBSCRIPTION_ID']
+TENANT_ID = os.environ['AZURE_TENANT']
+CLIENT_ID = os.environ['AZURE_CLIENT_ID']
+CLIENT_SECRET = os.environ['AZURE_SECRET']
+
+
+#--------------------------------------------------------------------------
+# variables
+#--------------------------------------------------------------------------
+AZURE_LOCATION = 'eastus'
+
+
+#--------------------------------------------------------------------------
+# management clients
+#--------------------------------------------------------------------------
+credentials = ServicePrincipalCredentials(
+    client_id=CLIENT_ID,
+    secret=CLIENT_SECRET,
+    tenant=TENANT_ID
+)
+mgmt_client = NetworkManagementClient(credentials, SUBSCRIPTION_ID)
+
+
+
+#--------------------------------------------------------------------------
+# /Operations/get/Get a list of operations for a resource provider[get]
+#--------------------------------------------------------------------------
+print("Get a list of operations for a resource provider")
+result = mgmt_client.operations.list()
